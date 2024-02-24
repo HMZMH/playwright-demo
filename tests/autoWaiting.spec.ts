33bf26bb5a5cc96f8bc1c405a3a29e6fbb210aac
Hamzah Mahmood <hmzmh2020@gmail.com>
2024-02-24 11:51:22 +0000

    first commit

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,50 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+})
+
+test('auto waiting', async({page}) => {
+    const successButton = page.locator('.bg-success')
+
+    // await successButton.click()
+
+    // const text = await successButton.textContent()
+
+    // const text = await successButton.allTextContents()
+
+    // expect(text).toEqual('Data loaded with AJAX get request')
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+})
+
+test('alternative waits', async({page}) => {
+    const successButton = page.locator('.bg-success')
+
+    // ___ wait for element
+    // await page.waitForSelector('.bg-success')
+
+    // __ wait for particular response
+    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+    // __ wait for network calls to be completed ('NOT RECOMMENDED')
+
+    await page.waitForLoadState('networkidle')
+
+    const text = await successButton.allTextContents()
+    expect(text).toContain('Data loaded with AJAX get request.')
+})
+
+// Default timeout is 30 seconds
+// Define timeouts in playwright.config.ts
+test('timeouts', async ({page}) => {
+
+    // test.setTimeout(10000)
+
+    // test.slow()
+    const successButton = page.locator('.bg-success')
+
+    await successButton.click({timeout: 15000}) //timeouts with click() will override timeouts defind in playwright.config.ts
+
+})
---
+++ tests/uiComponents.spec.ts
@@ -0,0 +1,106 @@
+import {test, expect} from '@playwright/test'
+import { assert } from 'console'
+
+test.beforeEach(async({page}) => {
+    await page.goto('hhtp://localhost:4200/')
+})
+
+test.describe('Form Layouts page', () => {
+    test.beforeEach( async({page}) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+    })
+
+    // Input fields
+    test('input fields', async({page}) => {
+        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
+
+        await usingTheGridEmailInput.fill('test@test.com')
+        await usingTheGridEmailInput.clear()
+        await usingTheGridEmailInput.pressSequentially('test2@test.com')
+
+        // Generic assertion
+        const inputValue = await usingTheGridEmailInput.inputValue()
+        expect(inputValue).toEqual('test2@test.com')
+
+        // Locator Assertion
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+    })
+
+    // Radio buttons
+    test('radio buttons', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force:true})
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect (usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+
+    })
+
+    // Checkboxes
+    test('checkboxes', async({page}) => {
+
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Toastr').click()
+
+        await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+        await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+        const allBoxes = page.getByRole('checkbox')
+        for (const box of await allBoxes.all()){
+            await box.uncheck({force: true})
+            expect(await box.isChecked()).toBeFalsy()
+        }
+    
+    })
+
+    // Web tables
+    test('web tables', async({page}) => {
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart Table').click()
+
+        // 1 get the row by any test in this row
+        const targetRow = page.getByRole('row', {name: "twitter@outlook.com"})
+        await targetRow.locator('.nb-edit').click()
+        await page.locator('input-editor').getByPlaceholder('Age').clear()
+        await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+        await page.locator('.nb-checkmark').click()
+
+        // 2 get the row based on the value in a specific column
+        await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+        const targetRowById = page.getByRole('row', {name: "11"}).filter({has: page.locator('td').nth(1).getByText('11')})
+        await targetRowById.locator('.nbedit').click()
+        await page.locator('input editor').getByPlaceholder('E-mail').clear()
+        await page.locator('input editor').getByPlaceholder('E-mail').fill('test@test.com')
+        await page.locator('.nb-checkmark').click()
+        await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+
+        // 3 test filer of the table
+
+        const ages = ["20", "30", "40", "200"]
+
+        for (let age of ages){
+            await page.locator('input-filter').getByPlaceholder('Age').clear()
+            await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+            await page.waitForTimeout(500)
+            const ageRows = page.locator('tbody tr')
+
+            for (let row of await ageRows.all()){
+                const cellValue = await row.locator('td').last().textContent()
+
+                if(age=="200"){
+                    expect(await page.getByRole('table').textContent()).toContain('No data found')
+                } else {
+                    expect(cellValue).toEqual(age)
+                }
+            }
+        }
+    })
+
+})
