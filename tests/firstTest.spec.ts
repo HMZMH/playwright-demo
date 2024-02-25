247b3ca537a93da1af228d26b742b4052bb0de9c
Hamzah Mahmood <hmzmh2020@gmail.com>
2024-02-25 15:58:52 +0000

    Typo fix

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -82,7 +82,7 @@ test('locating parent elements', async({page}) => {
 
     await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"}).getByRole('textbox', {name: "Email"}).click()
 
-    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Emaail"}).click()
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
 })
 
 
